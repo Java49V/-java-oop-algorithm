5590dbbd5d02966ea8a83d001ec36570630bfab4
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-05-02 21:18:39 +0300

    HW #8 binarySearch

--- src/telran/algorithm/InitialAlgorithms.java
+++ src/telran/algorithm/InitialAlgorithms.java
@@ -1,13 +1,13 @@
 package telran.algorithm;
 
-import java.util.Arrays;
+import java.util.Comparator;
 
 public class InitialAlgorithms {
 	public static void sortShortPositive(short[] array) {
 		int[] helper = new int[Short.MAX_VALUE];
-		// helper[index] => count of occurences for number index in array
-		// helper[1000] = 2 => number 1000 occurs twice times in the source array
-		// helper[2] = 0 => there is no number 2 in the given array;
+		// helper[index] => count of occurrences for number index in array
+		// helper[1000] = 2 => number 1000 occurs twice in the source array
+		// helper[2] = 0;
 		for (int i = 0; i < array.length; i++) {
 			helper[array[i]]++;
 		}
@@ -17,41 +17,131 @@ public class InitialAlgorithms {
 				array[ind++] = (short) i;
 			}
 		}
+
 	}
 
 	public static boolean isSum2(short[] array, short sum) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == sum) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+		// array of the positive short numbers
+		// returns true if there are two numbers in the given array,
+		// sum of which equals the given sum value
+		// otherwise false
+	
+		int helperSize = sum < 0 ? Short.MAX_VALUE + 1 : sum + 1;
+		boolean[] helper = new boolean[helperSize];
+		boolean res = false;
+		int index = 0;
+		while (index < array.length && !res) {
+			short value = array[index];
+			short secondValue =  (short) (sum - value);
+			if (secondValue >= 0) {
+				if (helper[secondValue]) {
+					res = true;
+				} else {
+					helper[value] = true;
+				}
+			}
+			index++;
+		}
+		return res;
+	}
 
-    public static short getMaxPositiveWithNegativeReflect(short[] array) {
-        short maxPositive = -1;
-        for (short num : array) {
-            if (num > 0 && Arrays.binarySearch(array, (short) -num) >= 0) {
-                maxPositive = num > maxPositive ? num : maxPositive;
-            }
-        }
-        return maxPositive;
-    }
+	public static short getMaxPositiveWithNegativeReflect(short[] array) {
+		// returns maximal positive number, for which there is the negative image
+		// If there are not such numbers at all the method returns -1
+		short res = -1;
+		byte[] helper = new byte[Short.MAX_VALUE];
+		short candidate = -1;
+		for (int i = 0; i < array.length; i++) {
+			candidate = (short) Math.abs(array[i]);
+			if (array[i] < 0) {
 
-    public static void bubbleSort(short[] array) {
-        int n = array.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    // Swap array[j] and array[j+1]
-                    short temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
-        }
-    }
+				res = getRes(res, helper, candidate, 1);
+			} else {
+				res = getRes(res, helper, candidate, -1);
+			}
+		}
+
+		return res;
+	}
+
+	private static short getRes(short res, byte[] helper, short candidate, int sign) {
+		if (helper[candidate] == 1 * sign && candidate > res) {
+			res = candidate;
+		} else if (helper[candidate] == 0) {
+			helper[candidate] =  (byte) (-1 * sign);
+		}
+		return res;
+	}
+
+	public static void bubbleSort(short[] array) {
+		// sorting with bubble sort algorithm
+		int size = array.length;
+		boolean flUnsorted = false;
+		do {
+			size--;
+			flUnsorted = false;
+			for (int i = 0; i < size; i++) {
+				if (array[i] > array[i + 1]) {
+					short tmp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = tmp;
+					flUnsorted = true;
+				}
+			}
+		} while (flUnsorted);
+
+	}
+//	public static <T> int binarySearch(T [] array, T key,
+//			Comparator<T> comp) {
+//		int leftIndex = 0;
+//		int rightIndex = array.length - 1;
+//		int middleIndex = rightIndex / 2;
+//		int compRes = 0;
+//		while(leftIndex <= rightIndex &&
+//				(compRes = comp.compare(key, array[middleIndex] )) != 0) {
+//			if (compRes > 0) {
+//				//move to right part of the array
+//				leftIndex = middleIndex + 1;
+//			} else {
+//				rightIndex = middleIndex - 1;
+//			}
+//			middleIndex = (leftIndex + rightIndex) / 2;
+//			
+//		}
+//		return leftIndex > rightIndex ? -1 : middleIndex;
+//	}
+	
+	public static <T> int binarySearch(T[] array, T key, Comparator<T> comp) {
+	    int leftIndex = 0;
+	    int rightIndex = array.length - 1;
+	    int middleIndex;
+	    int compRes;
+	    int result = -1;
+
+	    while (leftIndex <= rightIndex) {
+	        middleIndex = (leftIndex + rightIndex) / 2;
+	        compRes = comp.compare(key, array[middleIndex]);
+
+	        if (compRes < 0) {
+	            rightIndex = middleIndex - 1;
+	        } else if (compRes > 0) {
+	            leftIndex = middleIndex + 1;
+	        } else {
+	            // Key is found, search for the first occurrence
+	            result = middleIndex;
+	            rightIndex = middleIndex - 1;
+	        }
+	    }
+
+	    // If key is not found, calculate the insertion point
+	    if (result == -1) {
+	        result = -(leftIndex + 1);
+	    }
+
+	    return result;
+	}
 
 }
+
+
+
--- src/telran/algorithm/InitialAlgorithmsTest.java
+++ src/telran/algorithm/InitialAlgorithmsTest.java
@@ -2,94 +2,130 @@ package telran.algorithm;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static telran.algorithm.InitialAlgorithms.*;
+
 import java.util.Arrays;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import static telran.algorithm.InitialAlgorithms.*;
+import java.util.Comparator;
 
-class InitialAlgorithmTest {
 
+class InitialAlgorithmsTest {
+ int N_NUMBERS = 10000000;
+ short[] array;
+
+ void setUpBigArray() {
+	 
+	 array = new short[N_NUMBERS];
+	 for(int i = 0; i < N_NUMBERS; i++) {
+		 array[i] = (short) (Math.random() * Short.MAX_VALUE);
+	 }
+ }
 	@Test
-	void testGetMaxPositiveWithNegativeReflect() {
-		short[] numbers = { -50, -20, 7, 10, 30, 20, 50, 100, 0 };
-		assertEquals(50, InitialAlgorithms.getMaxPositiveWithNegativeReflect(numbers));
-
-		short[] numbersNoMatches_Neg = { 0, -1, -2, -3, -4, -5 };
-		assertEquals(-1, InitialAlgorithms.getMaxPositiveWithNegativeReflect(numbersNoMatches_Neg));
-
-		short[] numbersNoMatches_Pos = { 1, 2, 3, 4, 5 };
-		assertEquals(-1, InitialAlgorithms.getMaxPositiveWithNegativeReflect(numbersNoMatches_Pos));
+	@Disabled
+	void bubbleSortTest() {
+		setUpBigArray();
+		bubbleSort(array);
+		runTest();
+	}
+	@Test
+	void SortPositiveShortTest() {
+		setUpBigArray();
+		sortShortPositive(array);
+		runTest();
+	}
+	private void runTest() {
+		for(int i = 1; i < N_NUMBERS; i++) {
+			assertTrue(array[i - 1] <= array[i]);
+		}
 		
-		short[] array = {-3, 1, 4, -1, 3, -4};
-        short result = InitialAlgorithms.getMaxPositiveWithNegativeReflect(array);
-        System.out.println("getMaxPositiveWithNegativeReflect test:");
-        System.out.println(Arrays.toString(array) + " => " + result);
-
 	}
-
 	@Test
-	void testIsSum2() {
-		short[] numbers = { 0, 5, 1, 3, 2, 4 };
-		assertTrue(InitialAlgorithms.isSum2(numbers, (short) 5));
-
-		short[] numbersNoSum = { 0, 1, 2, 3, 4, 5 };
-		assertFalse(InitialAlgorithms.isSum2(numbersNoSum, (short) 0));
-
-		short[] numbersSumZero = { 0, 1, 2, 3, 4, 0 };
-		assertTrue(InitialAlgorithms.isSum2(numbersSumZero, (short) 0));
-
-		short[] numbersMiddleSum = { 8, 1, 2, 3, 3 };
-		assertTrue(InitialAlgorithms.isSum2(numbersMiddleSum, (short) 6));
+	void isSum2Test() {
+		short[] array = {30000, 1, 5, 2, 10000, 0, 500,0};
+		short[] array1 = {30000, 1, 5, 2, 10000, 0, 500,0, Short.MAX_VALUE};
+		assertTrue(isSum2(array, (short)30000));
+		assertTrue(isSum2(array, (short)7));
+		assertFalse(isSum2(array, (short)30003));
+		assertFalse(isSum2(array, (short)8));
+		assertTrue(isSum2(array1, Short.MIN_VALUE));
 		
-		short[] array = {1, 2, 3, 4, 5};
-        short sum = 7;
-        boolean result = InitialAlgorithms.isSum2(array, sum);
-        System.out.println("isSum2 test:");
-        System.out.println(Arrays.toString(array) + ", sum = " + sum + " => " + result);
 	}
-
 	@Test
-	void testBubbleSort() {
-		short[] randomArray = getRandomArray(10000);
-		InitialAlgorithms.bubbleSort(randomArray);
-		assertTrue(runSortedAscendingArrayTest(randomArray));
+	void getMaxPositiveWithNegativeTest() {
+		short[] array = {1, 1, 1, -1, 20, 100,200, 100 -100, -100, -20, -40, 80};
+		short[] array1 = {-40, 1, -40, -6, 2, 3, 40};
+		short[] array2 = {40, 1, 2, 3, 40, -30};
+		assertEquals(100,
+				getMaxPositiveWithNegativeReflect(array));
+		assertEquals(40,
+				getMaxPositiveWithNegativeReflect(array1));
+		assertEquals(-1,
+				getMaxPositiveWithNegativeReflect(array2));
 	}
-
 	@Test
-	void testSortShortPositive() {
-		short[] randomArray = getRandomArray(1000000);
-		InitialAlgorithms.sortShortPositive(randomArray);
-		assertTrue(runSortedAscendingArrayTest(randomArray));
+	@Disabled
+	void maxValueComplexityNTest() {
+		assertEquals(Long.MAX_VALUE, getMaxValueComplexityN());
+	}
+	@Test
+	void maxValueComplexityLogNTest() {
+		assertEquals(Long.MAX_VALUE, getMaxValueComplexityLogN());
+	}
+	
+
+	
+	private Long getMaxValueComplexityN() {
+		long res = 1;
+		while(res > 0) {
+			res++;
+		}
+		return res - 1;
+	}
+	private Long getMaxValueComplexityLogN() {
+		long res = 1;
+		while(res > 0) {
+			res *= 2;
+		}
+		return res - 1;
 	}
 	
 	@Test
-	void testSortMethods() {
-        short[] array = getRandomArray(100_000);
-        System.out.println("Sorting 100,000 numbers:");
-        runSortedAscendingArrayTest(array);
+	public static void main(String[] args) {
+        Integer[] array = {1, 3, 5, 7, 7, 9, 11, 13};
+        int key = 7;
 
-        array = getRandomArray(1_000_000);
-        System.out.println("\nSorting 1,000,000 numbers:");
-        runSortedAscendingArrayTest(array);
+        Comparator<Integer> comp = Comparator.naturalOrder();
+        int index = InitialAlgorithms.binarySearch(array, key, comp);
+
+        if (index >= 0) {
+            System.out.println("Key found at index: " + index);
+        } else {
+            int insertionPoint = -(index + 1);
+            System.out.println("Key not found. Insertion point: " + insertionPoint);
+        }
     }
+	
+	
+	@Test
+	void testBinarySearch() {
+		Integer[] array = {1, 3, 5, 7, 7, 9, 11, 13};
+        int key = 7;
 
-	private boolean runSortedAscendingArrayTest(short[] array) {
-		int counter = 0;
-		for (int i = 0; i < array.length - 1; i++) {
-			if (array[i] > array[i + 1]) {
-				counter++;
-			}
-		}
+        Comparator<Integer> comp = Comparator.naturalOrder();
+        int index = InitialAlgorithms.binarySearch(array, key, comp);
 
-		return counter == 0;
-
+        if (index >= 0) {
+            System.out.println("Key found at index: " + index);
+        } else {
+            int insertionPoint = -(index + 1);
+            System.out.println("Key not found. Insertion point: " + insertionPoint);
+        }
+		
 	}
 
-	private short[] getRandomArray(int size) {
-		short[] randomArray = new short[size];
-		for (int i = 0; i < randomArray.length; i++) {
-			randomArray[i] = (short) (Math.random() * Short.MAX_VALUE);
-		}
-		return randomArray;
-	}
 
 }
