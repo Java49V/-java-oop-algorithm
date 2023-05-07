5b7b54011c546b0863e84452ad0858781fc3b543
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-05-07 16:31:35 +0300

    HW #9 MemoryServiceTest

--- src/telran/algorithm/MemoryService.java
+++ src/telran/algorithm/MemoryService.java
@@ -1,18 +1,48 @@
 package telran.algorithm;
 
+import java.util.Arrays;
+
 public class MemoryService {
-public static int getMaxAvailableSize() {
-	int res = Integer.MAX_VALUE;
-	boolean running = true;
-	while (running) {
-		try {
-			byte[] array = new byte[res];
-			running = false;
-		} catch(OutOfMemoryError e) {
-			res /= 2;
-		}
-	}
-	return res;
-	
+    public static long getMaxAvailableSize() {
+        long low = 0;
+        long high = Integer.MAX_VALUE;
+
+        while (low <= high) {
+            long middle = low + (high - low) / 2;
+            long[] array;
+
+            try {
+                array = new long[(int) middle];
+                low = middle + 1;
+            } catch (OutOfMemoryError e) {
+                high = middle - 1;
+            }
+        }
+
+        return high;
+    }
+
+    public static void main(String[] args) {
+        long maxAvailableSize = getMaxAvailableSize();
+        System.out.println("Max Available Size: " + maxAvailableSize);
+    }
 }
-}
+
+
+
+//public class MemoryService {
+//public static int getMaxAvailableSize() {
+//	int res = Integer.MAX_VALUE;
+//	boolean running = true;
+//	while (running) {
+//		try {
+//			byte[] array = new byte[res];
+//			running = false;
+//		} catch(OutOfMemoryError e) {
+//			res /= 2;
+//		}
+//	}
+//	return res;
+//	
+//}
+//}
--- src/telran/algorithm/MemoryServiceTest.java
+++ src/telran/algorithm/MemoryServiceTest.java
@@ -4,20 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class MemoryServiceTest {
-byte[] array;
-	@Test
-	void test() {
-		int size = MemoryService.getMaxAvailableSize();
-		array = new byte[size];
-		boolean flException = false;
-		try {
-			array = null;
-			array = new byte[size + 1];
-		} catch (OutOfMemoryError e) {
-			flException = true;
-		}
-		assertTrue(flException);
-	}
+import org.junit.jupiter.api.Assertions;
 
+
+public class MemoryServiceTest {
+    @Test
+    public void testGetMaxAvailableSize() {
+        long maxAvailableSize = MemoryService.getMaxAvailableSize();
+        System.out.println("Max Available Size: " + maxAvailableSize);
+
+        Assertions.assertTrue(maxAvailableSize > 0, "Max Available Size should be greater than 0");
+
+        // Test small array allocation
+        long smallArraySize = maxAvailableSize / 10;
+        try {
+            long[] smallArray = new long[(int) smallArraySize];
+            Assertions.assertEquals(smallArraySize, smallArray.length, "Small array size should match requested size");
+        } catch (OutOfMemoryError e) {
+            Assertions.fail("Failed to allocate small array within available memory");
+        }
+
+        // Test large array allocation
+        long largeArraySize = maxAvailableSize * 2;
+        try {
+            long[] largeArray = new long[(int) largeArraySize];
+            Assertions.fail("Allocating large array should have resulted in OutOfMemoryError");
+        } catch (OutOfMemoryError e) {
+            // Allocation should fail due to insufficient memory
+            Assertions.assertTrue(true);
+        }
+    }
 }
+
